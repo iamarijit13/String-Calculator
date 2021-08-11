ded1ce131941e5d9467ce62aa29fa931b8d21524
ARIJIT <68080361+arispeak@users.noreply.github.com>
2021-08-11 21:50:05 +0530

    Second Test case added. To check  add() method can handle unknown amount of numbers.

--- src/test/java/com/StringCalculatorTest.java
+++ src/test/java/com/StringCalculatorTest.java
@@ -16,4 +16,11 @@ public class StringCalculatorTest {
         assertEquals(1, this.object.add("1"));
         assertEquals(3, this.object.add("1,2"));
     }
+
+    @Test 
+    public void test2 () {
+        assertEquals(13, this.object.add("1,5,7"));
+        assertEquals(27, this.object.add("10,2,3,5,7"));
+    }
+
 }
