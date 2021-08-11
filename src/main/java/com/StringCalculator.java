1a8048feb240e822f0c410d66db1f9b8c107feef
ARIJIT <68080361+arispeak@users.noreply.github.com>
2021-08-11 23:09:23 +0530

    StringCalculator class modified. A new method added to detect delimiter. Now add() can handle different delimiters.  All test cases passed successfully.

--- src/main/java/com/StringCalculator.java
+++ src/main/java/com/StringCalculator.java
@@ -4,18 +4,40 @@ package com;
 import java.util.*;
 
 public class StringCalculator {
+    private String in, de;
+
+    public StringCalculator () {
+        in = new String();
+        de = new String();
+    }
+
+    public void delimiterFinder () {
+    
+        Vector v = new Vector();
+
+        char[] ch = this.in.toCharArray();
+
+        this.de = String.valueOf(ch[3]);
+    }
+
     public int add (String input) {
         int count = 0;
+        this.in = input;
 
         if (input.isEmpty()) {
             return count;
         }
-
-        String []parts = input.split(",");
+        
+        String []parts = input.split(this.de);
         
         for ( String st : parts ) {
             String []part = st.split("\n");
             for (String i : part) {
+                try {
+                    Integer.parseInt(i);
+                } catch(NumberFormatException e) {
+                    continue;
+                }
                 count += Integer.parseInt(i);
             } 
         }
