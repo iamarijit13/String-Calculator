74364150bc35439cf213096492480c5a031fac0f
ARIJIT <68080361+arispeak@users.noreply.github.com>
2021-08-11 21:26:03 +0530

    add() method added to the StringCalculator class. Check for all “”==0, “1”==1, “1,2”==3 conditions, test passes successfully.

--- src/main/java/com/StringCalculator.java
+++ src/main/java/com/StringCalculator.java
@@ -4,5 +4,19 @@ package com;
 import java.util.*;
 
 public class StringCalculator {
-    
+    public int add (String input) {
+        int count = 0;
+
+        if (input.isEmpty()) {
+            return count;
+        }
+
+        String []parts = input.split(",");
+        
+        for ( String st : parts ) {
+            count += Integer.parseInt(st);
+        }
+
+        return count;
+    }
 }
